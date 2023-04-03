c6fb74ebd9b6d5e9f332a4a8909610756f81d722
Minh Hoàng Lê <leminhhoangk3@gmail.com>
2023-04-03 11:59:45 +0700

    implement babel loader, css loader, image loader

---
+++ js_modules/src/image_viewer.js
@@ -0,0 +1,18 @@
+import big from "../assets/big.jpg";
+import small from "../assets/small.jpg";
+import "../styles/image_viewer.css";
+
+// const image = document.createElement("img");
+// image.src = "https://placehold.co/400";
+
+// document.body.appendChild(image);
+
+const smallImage = document.createElement("img");
+smallImage.src = small;
+
+document.body.appendChild(smallImage);
+
+const bigImage = document.createElement("img");
+bigImage.src = big;
+
+document.body.appendChild(bigImage);
